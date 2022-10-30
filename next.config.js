4299b5ddebcf3144cc58e4af12b2cc3c5817423f
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-10-30 09:42:44 -0400

    v

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
-  experimental: {
-    // Required:
-    appDir: true,
-  },
+  
 };
 
 module.exports = nextConfig;
